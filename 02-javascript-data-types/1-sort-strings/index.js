15dfc23565bc917525d7f45a5005419d6d1bdf45
emakarov <emakarov@lanit.ru>
2023-09-24 21:50:33 +0300

    2. Основы JS. Примитивные типы данных

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,7 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+    return param === 'asc'
+        ? [...arr].sort((a, b) => a.localeCompare(b, 'ru-u-kf-upper'))
+        : [...arr].sort((a, b) => b.localeCompare(a, 'ru-u-kf-upper'));
 }
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,8 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+    return Object.entries(obj).reduce((result, [key, value]) => {
+        if (!fields.includes(key)) result[key] = value;
+        return result;
+    }, {});
 };
