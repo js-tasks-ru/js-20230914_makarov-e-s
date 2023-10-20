ae294196d6177fc5d0649fa9c3233e7a6cef5b04
emakarov <emakarov@lanit.ru>
2023-10-20 23:04:45 +0300

    7. Работа с асинхронным кодом. Fetch API ColumnChart компонент с динамической загрузкой данных

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,9 +1,38 @@
 export default class ColumnChart {
+    #element;
+    subElements;
 
     chartHeight = 50;
 
+    #loading = false;
+
+    set element(value) {
+        this.#element = value;
+        this.subElements = {
+            header: this.#element.querySelector('.column-chart__header'),
+            body: this.#element.querySelector('.column-chart__chart'),
+        };
+    }
+
+    get element() {
+        return this.#element;
+    }
+
+    set loading(value) {
+        this.#loading = value;
+        if (this.#loading) {
+            this.element.classList.add('column-chart_loading');
+        } else {
+            this.element.classList.remove('column-chart_loading');
+        }
+    }
+
+    get loading() {
+        return this.#loading;
+    }
+
     constructor(config) {
-        this.columnProps = this.getColumnProps(config?.data);
+        this.data = this.convertValuesToColumnData(config?.data);
         this.label = config?.label;
         this.value = config?.value;
         this.link = config?.link;
@@ -12,77 +41,64 @@ export default class ColumnChart {
         this.element = this.createElement();
     }
 
-    labelTemplate() {
-        return `
-            Total ${this.label}
-        `;
-    }
+    createElement() {
+        const element = document.createElement('div');
 
-    linkTemplate() {
-        return `
-            ${this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : ''}
-        `;
-    }
+        element.className = this.data?.length ? 'column-chart' : 'column-chart column-chart_loading';
+        element.innerHTML = this.template();
+        element.style.setProperty('--chart-height', this.chartHeight);
 
-    valueTemplate() {
-        return `
-            <div data-element="header" class="column-chart__header">
-                ${this.formatHeading ? this.formatHeading(this.value) : this.value}
-            </div>
-        `;
-    }
-
-    columnsTemplate() {
-        return `
-            <div data-element="body" class="column-chart__chart">
-                ${this.columnProps?.length ? this.columnProps.map(this.columnTemplate).join('\n') : ''}
-            </div>
-        `;
-    }
-
-    columnTemplate(prop) {
-        return `
-            <div style="--value: ${prop.value}" data-tooltip="${prop.percent}"></div>
-        `;
+        return element;
     }
 
     template() {
         return `
             <div class="column-chart__title">
-                ${this.labelTemplate()}
-                ${this.linkTemplate()}
+                Total ${this.label}
+                ${this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : ''}
             </div>
             <div class="column-chart__container">
-                ${this.valueTemplate()}
-                ${this.columnsTemplate()}
+                <div data-element="header" class="column-chart__header">
+                    ${this.convertValueToFormatValue(this.value)}
+                </div>
+                <div data-element="body" class="column-chart__chart">
+                    ${this.data?.length ? this.data.map(this.columnTemplate).join('\n') : ''}
+                </div>
             </div>
         `;
     }
 
-    createElement() {
-        const element = document.createElement('div');
-        element.style.setProperty('--chart-height', this.chartHeight);
-        element.className = !this.columnProps?.length
-            ? 'column-chart column-chart_loading'
-            : 'column-chart';
-
-        element.innerHTML = this.template();
-
-        return element;
+    columnTemplate(item) {
+        return `
+            <div style="--value: ${item.value}" data-tooltip="${item.percent}"></div>
+        `;
     }
 
-    render(container) {
+    update(values, total) {
+        this.data = this.convertValuesToColumnData(values);
+
+        this.subElements.header.innerHTML = this.convertValueToFormatValue(total);
+        this.subElements.body.innerHTML = this.data?.length
+            ? this.data.map(this.columnTemplate).join('\n')
+            : '';
+    }
+
+    convertValueToFormatValue(value) {
+        return this.formatHeading ? this.formatHeading(value) : value;
+    }
+
+    convertValuesToColumnData(values = []) {
+        const maxValue = Math.max(...values);
+        const scale = 50 / maxValue;
+      
+        return values.map(item => ({
+            percent: (item / maxValue * 100).toFixed(0) + '%',
+            value: String(Math.floor(item * scale)),
+        }));
+    }
+
+    render(container = document.body) {
         container.appendChild(this.element);
-    }
-
-    update(columnProps) {
-        this.columnProps = this.getColumnProps(columnProps);
-
-        const template = Object.assign(document.createElement('template'), {
-            innerHTML: this.columnsTemplate(),
-        })
-        
-        this.element.querySelector('.column-chart__chart').replaceWith(template);
     }
 
     remove() {
@@ -92,21 +108,4 @@ export default class ColumnChart {
     destroy() {
         this.remove();
     }
-
-    getColumnProps(data) {
-        if (!data || !data.length) {
-            return;
-        }
-
-        const maxValue = Math.max(...data);
-        const scale = 50 / maxValue;
-      
-        return data.map(item => {
-            return {
-                percent: (item / maxValue * 100).toFixed(0) + '%',
-                value: String(Math.floor(item * scale))
-            };
-        });
-    }
-      
 }
--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -1,7 +1,33 @@
 import fetchJson from './utils/fetch-json.js';
+import ColumnChartV1 from '../../04-oop-basic-intro-to-dom/1-column-chart/index.js';
 
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
-export default class ColumnChart {
+export default class ColumnChart extends ColumnChartV1 {
+    constructor(config) {
+        super(config);
 
+        this.url = new URL(config?.url, BACKEND_URL);
+        this.range = config?.range;
+
+        this.update(this.range?.from, this.range?.to);
+    }
+
+    async update(from, to) {
+        this.url.searchParams.set('from', from);
+        this.url.searchParams.set('to', to);
+
+        this.loading = true;
+
+        const response = await fetchJson(this.url);
+
+        this.loading = false;
+
+        const values = Object.values(response);
+        const total = values.reduce((acc, item) => acc + item);
+
+        super.update(values, total);
+
+        return response;
+    }
 }
